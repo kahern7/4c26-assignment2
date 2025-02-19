d28a8afa81a8ac398440a5c3d07f2d04b44c6186
kahern7 <kahern@tcd.ie>
2025-02-19 15:15:26 +0000

    Interim commit

--- controller_assignment2.py
+++ controller_assignment2.py
@@ -15,22 +15,39 @@ table={}
 
 rules=[
 # => you need now to add other rules to satisfy the assignment requirements. Notice that we will make decisions based on Ethernet address rather than IP address. Rate limiting is implemented by sending the pacet to the correct port and queue (the queues that you have specified in the topology file).
+{'priority':100,'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:03', 'TCPPort':40, 'queue':1, 'drop':False}, # cap at 30 Mb/s
+{'priority':100,'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:02', 'TCPPort':60, 'queue':1, 'drop':False}, # cap at 150 Mb/s
+       
+# ARP rule inserted for all ports outside those specifically mentioned in the assignment briefing
+{'priority':40,'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:03', 'queue':0, 'drop':False}, # ARP uncapped
+{'priority':40,'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:02', 'queue':0, 'drop':False}, # ARP uncapped
+
+{'priority':60,'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:04', 'queue':0, 'drop':False}, # uncapped
+{'priority':60,'EthSrc':'00:00:00:00:00:04','EthDst':'00:00:00:00:00:01', 'queue':None, 'drop':False}, # uncapped
+
+{'priority':80,'EthSrc':'00:00:00:00:00:02','EthDst':'00:00:00:00:00:04', 'queue':1, 'drop':False}, # cap at 200 Mb/s
+
+{'priority':80,'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:04', 'drop':True}, # blocked
+{'priority':80,'EthSrc':'00:00:00:00:00:04','EthDst':'00:00:00:00:00:03', 'drop':True}, # blocked
+
+{'priority':60,'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:01', 'queue':None, 'drop':False}, # uncapped
+{'priority':60,'EthSrc':'00:00:00:00:00:02','EthDst':'00:00:00:00:00:01', 'queue':None, 'drop':False}, # uncapped
+{'priority':60,'EthSrc':'00:00:00:00:00:04','EthDst':'00:00:00:00:00:02', 'queue':0, 'drop':False}, # uncapped
 ]
 
-    	# => the first two example of rules have been added for you, you need now to add other rules to satisfy the assignment requirements. Notice that we will make decisions based on Ethernet address rather than IP address. Rate limiting is implemented by sending the pacet to the correct port and queue (the queues that you have specified in the topology file).
-
-def launch ():
+    	
+def launch():
     core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
     core.openflow.addListenerByName("PacketIn",  _handle_PacketIn)
     log.info("Switch running.")
 
-def _handle_ConnectionUp ( event):
+def _handle_ConnectionUp(event):
     log.info("Starting Switch %s", dpidToStr(event.dpid))
     msg = of.ofp_flow_mod(command = of.OFPFC_DELETE)
     event.connection.send(msg)
 
 
-def _handle_PacketIn ( event): # Ths is the main class where your code goes, it will be called every time a packet is sent from the switch to the controller
+def _handle_PacketIn(event): # Ths is the main class where your code goes, it will be called every time a packet is sent from the switch to the controller
 
     dpid = event.connection.dpid
     sw=dpidToStr(dpid)
@@ -44,16 +61,31 @@ def _handle_PacketIn ( event): # Ths is the main class where your code goes, it 
     # ==> write code here that associates the given port with the sending node using the source address of the incoming packet
     # ==> if available in the table this line determines the destination port of the incoming packet
 
+    table[(dpid, eth_packet.src)] = event.port   # this associates the given port with the sending node using the source address of the incoming packet
+    dst_port = table.get((dpid, eth_packet.dst)) # if available in the table this line determines the destination port of the incoming packet
 
     # this part is now separate from next part and deals with ARP messages
 
     if dst_port is None and eth_packet.type == eth.ARP_TYPE and eth_packet.dst == EthAddr(b"\xff\xff\xff\xff\xff\xff"):
         # => in this case you want to create a packet so that you can send the message as a broadcast
+        data_msg = of.ofp_packet_out(data = event.ofp)
+        data_msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
+        event.connection.send(data_msg)
 
     for rule in rules: #now you are adding rules to the flow tables like before. First you check whether there is a rule match based on Eth source and destination
-        if...# => now you are adding rules to the flow tables like before. First you check whether there is a rule match based on Eth source and destination as in assignment 1, but you need to adapt this for the case of more than 1 switch
+        if eth_packet.dst==EthAddr(rule['EthDst']) and eth_packet.src==EthAddr(rule['EthSrc']):
+            log.debug("Event: found rule from source %s to dest  %s" % (eth_packet.src, eth_packet.dst))
+            # => now you are adding rules to the flow tables like before. First you check whether there is a rule match based on Eth source and destination as in assignment 1, but you need to adapt this for the case of more than 1 switch
+            flow_msg = of.ofp_flow_mod()
+            flow_msg.priority = rule['priority']
+            flow_msg.match.dl_dst = eth_packet.dst
+            flow_msg.match.dl_src = eth_packet.src
+            flow_msg.soft_timeout = 40
 
-            if ...
+            # extract IP packet header
+            ip_packet = eth_packet.payload
+
+            if eth_packet.type != '0x0800' or ip_packet.protocol != '0x06':
             # => now check if the rule contains also TCP port info. If not install the flow without any port restriction
                 # => also remember to check if this is a drop rule. The drop function can be added by not sending any action to the flow rule
                 # => also remember that if there is a QoS requirement, then you need to use the of.ofp_action_enqueue() function, instead of the of.ofp_action_output
--- topo_assignment2.py
+++ topo_assignment2.py
@@ -12,16 +12,27 @@ def assignmentTopo():
     ############ CODE SHOULD ONLY BE ADDED BELOW  #################################
     info( '*** Adding controller\n' )
      # =>add the controller here
+     net.addController('c0', controller=RemoteController)
 
     info( '*** Adding hosts\n' )
     h1 = net.addHost( 'h1', ip='10.0.0.1', mac='00:00:00:00:00:01' )
     # =>h1 is already added, now add the other hosts here
+    h2 = net.addHost( 'h2', ip='10.0.0.2', mac='00:00:00:00:00:02' )
+    h3 = net.addHost( 'h3', ip='10.0.0.3', mac='00:00:00:00:00:03' )
+    h4 = net.addHost( 'h4', ip='10.0.0.4', mac='00:00:00:00:00:04' )
 
     info( '*** Adding switches\n' )
     # =>add the switches here's2' )
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
 
     info( '*** Creating links\n' )
     # =>create the links here
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s2)
+    net.addLink(h4, s2)
+    net.addLink(s1, s2)
 
 
     info( '*** Starting network\n')
@@ -29,11 +40,11 @@ def assignmentTopo():
     h1, h2, h3, h4 = net.hosts[0],net.hosts[1],net.hosts[2],net.hosts[3]
 
     # =>fill in here command to add two queues to the correct port of the correct switch: Q0 with min rate 20000000 and max rate -SELECT THE PROPER RATE-, Q1 with min rate 50000000 and max rate -SELECT THE PROPER RATE-
-
+    os.system('sudo ovs-vsctl set port s1-eth2 qos=@newqos -- --id=@newqos create qos type=linux-htb queues=0=@q0,1=@q1 -- --id=@q0 create queue other-config:min-rate=50000000 other-config:max-rate=1000000000000 --  --id=@q1 create queue other-config:min-rate=20000000 other-config:max-rate=150000000')
     # =>fill in here command to add one queue to the correct port of the correct switch: Q0 with min rate 20000000 and max rate -SELECT THE PROPER RATE-
-   
+    os.system('sudo ovs-vsctl set port s2-eth3 qos=@newqos -- --id=@newqos create qos type=linux-htb queues=0=@q0,1=@q1 -- --id=@q0 create queue other-config:min-rate=50000000 other-config:max-rate=1000000000000 --  --id=@q1 create queue other-config:min-rate=20000000 other-config:max-rate=30000000')
     # =>fill in here command to add two queues to the correct port of the correct switch: you decide number of queues and rates
-
+    os.system('sudo ovs-vsctl set port s2-eth4 qos=@newqos -- --id=@newqos create qos type=linux-htb queues=0=@q0,1=@q1 -- --id=@q0 create queue other-config:min-rate=50000000 other-config:max-rate=1000000000000 --  --id=@q1 create queue other-config:min-rate=20000000 other-config:max-rate=200000000')
 
 
     ########### THIS IS THE END OF THE AREA WHERE YOU NEED TO ADD CODE ##################################
